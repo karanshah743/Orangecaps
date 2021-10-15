9d5db64d7721cd35a35a90930709ed86db24758a
Mann-tech13 <mannshah.ict19@gmail.com>
2021-10-16 02:17:37 +0530

    Navigation in progress

--- oc-website/src/App.js
+++ oc-website/src/App.js
@@ -5,19 +5,25 @@ import Team from "./pages/Team/team";
 import Feedback from "./pages/Feedback/feedback";
 import Events from "./pages/Events/event";
 import Contact from "./pages/ContactUs/contact";
-import Blogs from"./pages/Blogs/blog";
+import Blogs from "./pages/Blogs/blog";
+import Navigation from './components/navigation';
+import News from "./pages/News/news"
 
 function App() {
   return (
     <div className="App">
       <Router>
+
+        <Navigation/>
+
         <Switch>
-          <Route path="/home" component={ Home } exact/>
+          <Route path="/" component={ Home } exact/>
           <Route path="/team" component={ Team } exact/>
           <Route path="/feedback" component={ Feedback } exact/>
           <Route path="/events" component={ Events } exact/>
-          <Route path="/contact" component={ Contact } exact/>
+          <Route path="/contact" component={ Contact } exact/> 
           <Route path="/blogs" component={ Blogs } exact/>
+          <Route path="/news" component={ News } exact/>
         </Switch>
       </Router>
     </div>
---
+++ oc-website/src/components/navigation.js
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import "./navigation.css"
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Home from '../pages/Home/home';
+import Team from "../pages/Team/team";
+import Feedback from "../pages/Feedback/feedback";
+import Events from "../pages/Events/event";
+import Contact from "../pages/ContactUs/contact";
+import Blogs from "../pages/Blogs/blog";
+import News from "../pages/News/news"
+
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function Navigation() {
+
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+
+  return (
+    <div>
+      <div>
+        <Box sx={{ width: '100%' }}>
+
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs className="box-model" value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="Home" {...a11yProps(0)} />
+              <Tab label="Events" {...a11yProps(1)} />
+              <Tab label="Team" {...a11yProps(2)} />
+              <Tab label="Contact Us" {...a11yProps(3)} />
+              <Tab label="Blogs" {...a11yProps(4)} />
+              <Tab label="News" {...a11yProps(5)} />
+              <Tab label="Feedback" {...a11yProps(6)} />
+            </Tabs>
+          </Box>
+
+          <TabPanel value={value} index={0}>
+
+          </TabPanel>
+
+          <TabPanel value={value} index={1}>
+            <Events/>
+          </TabPanel>
+
+          <TabPanel value={value} index={2}>
+            <Team/>
+          </TabPanel>
+
+          <TabPanel value={value} index={3}>
+            <Contact/>
+          </TabPanel>
+
+          <TabPanel value={value} index={4}>
+            <Blogs/>
+          </TabPanel>
+
+          <TabPanel value={value} index={5}>
+            <News/>
+          </TabPanel>
+
+          <TabPanel value={value} index={5}>
+            <Feedback/>
+          </TabPanel>
+
+        </Box>        
+      </div>
+    </div>
+  )
+}
+
+export default Navigation
+
+
+
+
+
+
+
+
+
+// export default function BasicTabs() {
+
+
+//   return (
+//     <Box sx={{ width: '100%' }}>
+//       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+//         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+//           <Tab label="Item One" {...a11yProps(0)} />
+//           <Tab label="Item Two" {...a11yProps(1)} />
+//           <Tab label="Item Three" {...a11yProps(2)} />
+//         </Tabs>
+//       </Box>
+//       <TabPanel value={value} index={0}>
+//         Item One
+//       </TabPanel>
+//       <TabPanel value={value} index={1}>
+//         Item Two
+//       </TabPanel>
+//       <TabPanel value={value} index={2}>
+//         Item Three
+//       </TabPanel>
+//     </Box>
+//   );
+// }
