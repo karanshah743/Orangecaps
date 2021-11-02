6ff0fd606dbf4d877254410a83216cb45d2917d1
Mann-tech13 <mannshah.ict19@gmail.com>
2021-11-03 01:52:15 +0530

    page linking

--- oc-website/src/components/navigation.js
+++ oc-website/src/components/navigation.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { NavLink, Link } from "react-router-dom";
 import "./navigation.css"
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
@@ -17,68 +18,65 @@ import News from "../pages/News/news";
 
 
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+// function TabPanel(props) {
+//   const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && (
+//         <Box sx={{ p: 3 }}>
+//           <Typography>{children}</Typography>
+//         </Box>
+//       )}
+//     </div>
+//   );
+// }
 
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
+// TabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.number.isRequired,
+//   value: PropTypes.number.isRequired,
+// };
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
+// function a11yProps(index) {
+//   return {
+//     id: `simple-tab-${index}`,
+//     'aria-controls': `simple-tabpanel-${index}`,
+//   };
+// }
 
 function Navigation() {
 
-    const [value, setValue] = useState(0);
+    // const [value, setValue] = useState(0);
 
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
+    // const handleChange = (event, newValue) => {
+    //   setValue(newValue);
+    // };
 
   return (
     <div>
       <div className="navigation">
         {/* <div>
-          <img className="NavLogo" src={ Img } alt="" />
-        </div> */}
-        <div>
           <Box sx={{ width: '100%' }}>
 
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
               
               <Tabs className="box-model" value={value} onChange={handleChange} aria-label="basic tabs example">
                 
-                <Tab className="navi-name" label="Home" {...a11yProps(0)} />
-                <Tab className="navi-name" label="Events" {...a11yProps(1)} />
-                <Tab className="navi-name" label="Team" {...a11yProps(2)} />
-                <Tab className="navi-name" label="Contact-Us" {...a11yProps(3)} />
-                <Tab className="navi-name" label="Blogs" {...a11yProps(4)} />
-                <Tab className="navi-name" label="News" {...a11yProps(5)} />
-                <Tab className="navi-name" label="Feedback" {...a11yProps(6)} />
+                <Link className="link" to="/"><Tab className="navi-name" label="Home" {...a11yProps(0)} /></Link>
+                <NavLink to="/events"><Tab className="navi-name" label="Events" {...a11yProps(1)} /></NavLink>
+                <NavLink to="/team"><Tab className="navi-name" label="Team" {...a11yProps(2)} /></NavLink>
+                <NavLink to="/contact"><Tab className="navi-name" label="Contact-Us" {...a11yProps(3)} /></NavLink>
+                <NavLink to="/blogs"><Tab className="navi-name" label="Blogs" {...a11yProps(4)} /></NavLink>
+                <NavLink to="/news"><Tab className="navi-name" label="News" {...a11yProps(5)} /></NavLink>
+                <NavLink to="/feedback"><Tab className="navi-name" label="Feedback" {...a11yProps(6)} /></NavLink>
               </Tabs>
             </Box>
 
@@ -111,6 +109,15 @@ function Navigation() {
             </TabPanel>
 
           </Box>        
+        </div> */}
+        <div className="navMenu">
+          <NavLink className="link" activeClassName="is-active" to="/" exact> Home </NavLink>
+          <NavLink className="link" activeClassName="is-active" to="/events"> Events </NavLink>        
+          <NavLink className="link" activeClassName="is-active" to="/team"> Team </NavLink>
+          <NavLink className="link" activeClassName="is-active" to="/contact"> Contact </NavLink>
+          <NavLink className="link" activeClassName="is-active" to="/blogs"> Blogs </NavLink>
+          <NavLink className="link" activeClassName="is-active" to="/news"> News </NavLink>
+          <NavLink className="link" activeClassName="is-active" to="/feedback"> Feedback </NavLink>
         </div>
       </div>
 
